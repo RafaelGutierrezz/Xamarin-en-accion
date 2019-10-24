4e6d5750f824a295dca13efd7cd7dc20742dc24b
RafaelGutierrezz <36318470+RafaelGutierrezz@users.noreply.github.com>
2019-10-24 14:22:28 -0500

    Capitulo 4 con ejercicio

---
+++ Surveys.Core/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Surveys.Core.Views;
+namespace Surveys.Core
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new Views.Surveys());
+        }
+    }
+}
